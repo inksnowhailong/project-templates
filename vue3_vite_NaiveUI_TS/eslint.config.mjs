a5a7977ab436c7c1bf7306225af979ef1aa8ee42
张海龙 <inksnowhailong@gmail.com>
2025-01-06 18:18:11 +0800

    更新

---
+++ vue3_vite_NaiveUI_TS/eslint.config.mjs
@@ -0,0 +1,129 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
+import globals from "globals";
+import parser from "vue-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "**/.DS_Store",
+        "**/node_modules/",
+        "**/dist",
+        "**/dist-ssr",
+        "**/*.local",
+        "**/package-lock.json",
+        "**/components.ts",
+        "**/components.d.ts",
+        "**/logs",
+        "**/*.log",
+        "**/npm-debug.log*",
+        "**/yarn-debug.log*",
+        "**/yarn-error.log*",
+        "**/pnpm-debug.log*",
+        "**/lerna-debug.log*",
+        "**/.vscode/*",
+        "!**/.vscode/extensions.json",
+        "**/.idea",
+        "**/*.suo",
+        "**/*.ntvs*",
+        "**/*.njsproj",
+        "**/*.sln",
+        "**/*.sw?",
+        "**/auto-import.d.ts",
+        "**/pnpm-lock.yaml",
+        "**/yarn.lock",
+        "**/.eslintcache"
+    ],
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+        import: fixupPluginRules(_import),
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+            jest: "readonly",
+            window: true,
+        },
+
+        parser: parser,
+        ecmaVersion: 5,
+        sourceType: "module",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+
+            ecmaFeatures: {
+                jsx: true,
+                tsx: true,
+            },
+        },
+    },
+
+    rules: {
+        "no-console": "off",
+        "no-debugger": "off",
+        "no-var": 2,
+        "prefer-const": 2,
+        "no-unused-vars": 0,
+        "arrow-parens": 0,
+        "comma-dangle": 0,
+        "no-underscore-dangle": 0,
+        "no-undef": 0,
+        camelcase: 0,
+        quotes: [0, "single"],
+        "quote-props": [0, "always"],
+        semi: [0, "always"],
+        "max-classes-per-file": [0, "always"],
+        "linebreak-style": [0, "windows"],
+        "no-param-reassign": 0,
+        "no-plusplus": 0,
+        "max-len": 0,
+        "@typescript-eslint/no-explicit-any": ["off"],
+
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            varsIgnorePattern: ".*",
+            args: "none",
+        }],
+
+        "vue/multi-word-component-names": "off",
+        "vue/no-v-html": "off",
+        "vue/attributes-order": "off",
+        "vue/attribute-hyphenation": "off",
+        "vue/v-on-event-hyphenation": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "vue/require-default-prop": "off",
+        "vue/comment-directive": "off",
+        "@typescript-eslint/no-namespace": "off",
+    },
+}, {
+    files: ["**/__tests__/*.{j,t}s?(x)", "**/tests/unit/**/*.spec.{j,t}s?(x)"],
+
+    languageOptions: {
+        globals: {
+            ...globals.jest,
+        },
+    },
+}];
--- vue3_vite_NaiveUI_TS/vite.config.ts
+++ vue3_vite_NaiveUI_TS/vite.config.ts
@@ -60,12 +60,6 @@ export default defineConfig({
       }
     }
   },
-  test: {
-    // 启用类似 jest 的全局测试 API
-    globals: true,
-    // 使用 happy-dom 模拟 DOM
-    environment: 'happy-dom'
-  },
   build: {
     rollupOptions: {
       output: {
